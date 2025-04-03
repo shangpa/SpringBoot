743ac9ffb7fd5c62856264912d1e926504f5dee1
COM4-main\User <bcucs20@gmail.com>
2025-04-03 10:18:08 +0900

    second commit

--- src/main/java/com/Lect/week03/ContextController.java
+++ src/main/java/com/Lect/week03/ContextController.java
@@ -1,6 +1,7 @@
 package com.Lect.week03;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +17,24 @@ public class ContextController {
 		xmlContext = new XmlWebApplicationContext();
 		configContext = new AnnotationConfigWebApplicationContext();
 		mav = new ModelAndView();
+	}
+	//3주차 실습
+	@GetMapping
+	public ModelAndView beanTest() {
+		//컨텍스트 초기화
+		xmlContext.setConfigLocation("classpath:static/xml/Ex1.xml");
+		xmlContext.refresh();
+		SmsSender xmlSms = (SmsSender)xmlContext.getBean("xmlSms");
+		
+		//java Config 파일 등록
+		configContext.register(AppConfig.class);
+		configContext.refresh();
+		SmsSender configSms = (SmsSender)configContext.getBean("configSms");
+		
+		mav.setViewName("week03/beanView");//생성자에 view 이름을 지정하지 않은 경우
+		mav.addObject("xmlSms", xmlSms);//모델 객체를 전달
+		mav.addObject("configSms", configSms); // 모델 객체를 전달
+		return mav;
 	}
 
 }
